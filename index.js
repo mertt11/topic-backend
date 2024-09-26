b500c1557c329616e5acf19d3b8df274f80d1bc9
mertt11 <mertdeneme10@outlook.com>
2024-09-26 20:12:04 +0300

    deleted previous

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 app.use(express.json());
-
-const cors = require('cors')
-app.use(cors)
+app.use(cors());
 
 let notes = [
   {
@@ -22,16 +21,6 @@ let notes = [
     important: true,
   },
 ];
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-app.use(requestLogger)
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -79,6 +68,5 @@ app.post("/api/notes", (request, response) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
